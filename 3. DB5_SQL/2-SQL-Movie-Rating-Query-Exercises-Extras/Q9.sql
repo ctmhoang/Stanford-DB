a3977baf197368fc15037a6eba707b88b4b3202e
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-10-08 16:16:12 -0700

    Add comment in Q9

--- 3. DB5_SQL/2-SQL-Movie-Rating-Query-Exercises-Extras/Q9.sql
+++ 3. DB5_SQL/2-SQL-Movie-Rating-Query-Exercises-Extras/Q9.sql
@@ -1,3 +1,6 @@
+-- Some directors directed more than one movie. For all such directors, return the titles of all movies directed by them, along with the director name. Sort by director name, then movie title. (As an extra challenge, try writing the query both with and without COUNT.)
+-- 
+-- Note: Your queries are executed using SQLite, so you must conform to the SQL constructs supported by SQLite. 
 select title, director
 from Movie join (select distinct Mv.director
 from Movie Mv,(select director, mID
