7fff52344e0fbf058bdd6c5f6c14ee948a050dbb
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-10-08 16:33:14 -0700

    Forgot to add comment to Q10 -_-

--- 3. DB5_SQL/2-SQL-Movie-Rating-Query-Exercises-Extras/Q10.sql
+++ 3. DB5_SQL/2-SQL-Movie-Rating-Query-Exercises-Extras/Q10.sql
@@ -1,3 +1,9 @@
+--  Q10
+-- 1/1 point (ungraded)
+-- Find the movie(s) with the highest average rating. Return the movie title(s) and average rating. (Hint: This query is more difficult to write in SQLite than other systems; you might think of it as finding the highest average rating and then choosing the movie(s) with that average rating.)
+-- 
+-- Note: Your queries are executed using SQLite, so you must conform to the SQL constructs supported by SQLite. 
+
 select title , stars
 from Movie join
 (
